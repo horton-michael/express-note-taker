299697d3e0fc0b942c65805ceb09c3097e6f0d9c
mhorton <horton-michael@users.noreply.github.com>
2023-09-22 16:14:30 -0400

    fixes notes.html js and css links, adds middleware to serve static files in public folder

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app = express();
 PORT = process.env.PORT || 3000;
 
 // MIDDLEWARES
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(notesRouter);
